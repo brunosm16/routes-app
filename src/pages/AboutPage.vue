cd4b0296d3213c2cd018afb0332b5adfeddd848f
brunosm16 <brunoskr23@gmail.com>
2022-08-30 21:52:40 -0300

    change pages classes and directory.

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <span>About Component</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="contact">
     <span>About Component</span>
   </div>
 </template>
