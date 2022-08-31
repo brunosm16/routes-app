d54f74a4fe6ddded4812419e5a5cc98dc5c61afa
brunosm16 <brunoskr23@gmail.com>
2022-08-30 21:51:30 -0300

    about-page component.

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <span>About Component</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+};
+</script>
+
+<style scoped lang="scss"></style>
