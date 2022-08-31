836da20e3596af1e164b73b03a1897db0c388c09
brunosm16 <brunoskr23@gmail.com>
2022-08-30 21:50:51 -0300

    home-page component.

---
+++ src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <span>Home Component</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped lang="scss"></style>
