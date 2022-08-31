ccda2544cd310fa70395813d5ed7395b1a8ee20a
brunosm16 <brunoskr23@gmail.com>
2022-08-30 22:15:19 -0300

    navigation-bar styles.

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,17 +1,13 @@
 <template>
-  <nav class="nav">
-    <router-link to="/"> Home </router-link>
+  <div class="nav-wrapper">
+    <nav class="nav-wrapper__nav">
+      <router-link to="/"> Home </router-link>
 
-    <span class="nav__separator"> | </span>
+      <router-link to="/about"> About </router-link>
 
-    <router-link to="/about"> About </router-link>
-
-    <span class="nav__separator"> | </span>
-
-    <router-link to="/contact"> Contact </router-link>
-
-    <span class="nav__separator"> | </span>
-  </nav>
+      <router-link to="/contact"> Contact </router-link>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -20,4 +16,29 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.nav-wrapper {
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+
+  &__nav {
+    a {
+      font-size: 1.75rem;
+      margin-right: 3rem;
+      text-decoration: none;
+      transition: all 0.3s ease-in;
+
+      &:hover {
+        color: #319DA0 !important;
+      }
+    }
+
+    a.router-link-exact-active {
+      color: #42855b;
+    }
+  }
+}
+</style>
